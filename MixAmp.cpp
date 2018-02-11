567edf1e3875492e457c7ae178be6f1100bd1cb8
Pierce Nichols <pierce@logos-electro.com>
2018-02-10 22:38:21 -0800

    All features implemented and compiling -- no tests yet

--- MixAmp.cpp
+++ MixAmp.cpp
@@ -6,144 +6,203 @@
 /************************************************************/
 
 #include <iterator>
+#include <cstdlib>
 #include "MBUtils.h"
 #include "ACTable.h"
 #include "MixAmp.h"
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/schema.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/error/en.h"
+#include "schema/mixer_schema.h"
 
 using namespace std;
+using namespace rapidjson;
 
-//---------------------------------------------------------
-// Constructor
-
-MixAmp::MixAmp()
-{
+MixAmpInput::MixAmpInput (rapidjson::Value &v) {
+        var = v["name"].GetString();
+        if (v.HasMember("gain")) gain = v["gain"].GetDouble();
+        if (v.HasMember("offset")) offset = v["offset"].GetDouble();
 }
 
-//---------------------------------------------------------
-// Destructor
+bool MixAmpInput::procMail(CMOOSMsg &msg) {
+    if ((msg.GetKey() == var) && (msg.IsDouble())) {
+        val = msg.GetDouble();
+        return true;
+    } else {
+        return false;
+    }
+}
 
-MixAmp::~MixAmp()
-{
+Mixer::Mixer(rapidjson::Value &v) {
+    var = v["output"]["name"].GetString();
+    if (v["output"].HasMember("gain")) gain = v["output"]["gain"].GetDouble();
+    if (v["output"].HasMember("offset")) offset = v["output"]["offset"].GetDouble();
+    for (auto &i: v["inputs"].GetArray()) {
+        inputs.emplace_back(i);
+    }
+}
+
+bool Mixer::procMail(CMOOSMsg &msg) {
+    bool result = false;
+    for (auto &i: inputs) {
+        result |= i.procMail(msg);
+    }
+    return result;
+}
+
+std::list<std::string> Mixer::buildReportHeader() {
+    std::list<std::string> headers;
+    headers.push_back("Output: " + var);
+    for (auto &i: inputs) {
+        headers.push_back("Input: " + i.getVar());
+    }
+}
+
+std::list<std::string> Mixer::buildReportLines() {
+    std::list<std::string> headers;
+    headers.push_back(to_string(cooked()));
+    for (auto &i: inputs) {
+        headers.push_back(to_string(i.cooked()));
+    }
 }
 
 //---------------------------------------------------------
 // Procedure: OnNewMail
 
-bool MixAmp::OnNewMail(MOOSMSG_LIST &NewMail)
-{
-  AppCastingMOOSApp::OnNewMail(NewMail);
+bool MixAmp::OnNewMail(MOOSMSG_LIST &NewMail) {
+    AppCastingMOOSApp::OnNewMail(NewMail);
 
-  MOOSMSG_LIST::iterator p;
-  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
-    CMOOSMsg &msg = *p;
-    string key    = msg.GetKey();
-
-#if 0 // Keep these around just for template
-    string comm  = msg.GetCommunity();
-    double dval  = msg.GetDouble();
-    string sval  = msg.GetString(); 
-    string msrc  = msg.GetSource();
-    double mtime = msg.GetTime();
-    bool   mdbl  = msg.IsDouble();
-    bool   mstr  = msg.IsString();
-#endif
-
-     if(key == "FOO") 
-       cout << "great!";
-
-     else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
-       reportRunWarning("Unhandled Mail: " + key);
-   }
-	
-   return(true);
+    for (auto &msg: NewMail) {
+        string key = msg.GetKey();
+        bool result = false;
+        for (auto &m: mixers) {
+            bool tmp = m.procMail(msg);
+            if (tmp) m.transmit(this);
+            result |= tmp;
+        }
+        if(!result || (key != "APPCAST_REQ")) { // handled by AppCastingMOOSApp
+            reportRunWarning("Unhandled Mail: " + key);
+        }
+    }
+    return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: OnConnectToServer
 
-bool MixAmp::OnConnectToServer()
-{
-   registerVariables();
-   return(true);
+bool MixAmp::OnConnectToServer() {
+    registerVariables();
+    return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: Iterate()
 //            happens AppTick times per second
 
-bool MixAmp::Iterate()
-{
-  AppCastingMOOSApp::Iterate();
-  // Do your thing here!
-  AppCastingMOOSApp::PostReport();
-  return(true);
+bool MixAmp::Iterate() {
+    AppCastingMOOSApp::Iterate();
+    AppCastingMOOSApp::PostReport();
+    return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: OnStartUp()
 //            happens before connection is open
 
-bool MixAmp::OnStartUp()
-{
-  AppCastingMOOSApp::OnStartUp();
-
-  STRING_LIST sParams;
-  m_MissionReader.EnableVerbatimQuoting(false);
-  if(!m_MissionReader.GetConfiguration(GetAppName(), sParams))
-    reportConfigWarning("No config block found for " + GetAppName());
-
-  STRING_LIST::iterator p;
-  for(p=sParams.begin(); p!=sParams.end(); p++) {
-    string orig  = *p;
-    string line  = *p;
-    string param = toupper(biteStringX(line, '='));
-    string value = line;
-
-    bool handled = false;
-    if(param == "FOO") {
-      handled = true;
+bool MixAmp::OnStartUp() {
+    AppCastingMOOSApp::OnStartUp();
+    Document sd;
+    // check that we can load and parse the
+    if (sd.Parse(reinterpret_cast<char*>(mixer_schema_json), mixer_schema_json_len).HasParseError()) {
+        cerr << "Parse error " << GetParseError_En(sd.GetParseError());
+        cerr << " in JSON schema at offset " << sd.GetErrorOffset() << endl;
+        std::abort();
     }
-    else if(param == "BAR") {
-      handled = true;
+    SchemaDocument schemadoc(sd);
+    SchemaValidator validator(schemadoc);
+
+    STRING_LIST sParams;
+    m_MissionReader.EnableVerbatimQuoting(false);
+    if(!m_MissionReader.GetConfiguration(GetAppName(), sParams)) {
+        reportConfigWarning("No config block found for " + GetAppName());
     }
 
-    if(!handled)
-      reportUnhandledConfigWarning(orig);
+    for(auto &p: sParams) {
 
-  }
-  
-  registerVariables();	
-  return(true);
+        string orig  = p;
+        string line  = p;
+        string param = toupper(biteStringX(line, '='));
+        string value = line;
+
+        bool handled = false;
+        if("MIXER" == param) {
+            rapidjson::Document d;
+            if (d.Parse(value.c_str()).HasParseError()) {
+                cerr << "JSON parse error " << GetParseError_En(d.GetParseError());
+                cerr << " in " << param << " at offset " << d.GetErrorOffset() << endl;
+                std::abort();
+            } else {
+                if (d.Accept(validator)) {
+                    mixers.emplace_back(d);
+                } else {
+                    StringBuffer buf;
+                    Writer<StringBuffer> writer(buf);
+                    d.Accept(writer);
+                    cerr << "JSON failed validation:" << endl;
+                    cerr << "\t" << buf.GetString() << endl;
+                    validator.GetInvalidSchemaPointer().StringifyUriFragment(buf);
+                    cerr << "Invalid schema: " << buf.GetString() << endl;
+                    cerr << "Invalid keyword: " << validator.GetInvalidSchemaKeyword() << endl;
+                    std::abort();
+                }
+            }
+            handled = true;
+        }
+
+        if(!handled)
+          reportUnhandledConfigWarning(orig);
+
+    }
+
+    registerVariables();
+    return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: registerVariables
 
-void MixAmp::registerVariables()
-{
-  AppCastingMOOSApp::RegisterVariables();
-  // Register("FOOBAR", 0);
+void MixAmp::registerVariables() {
+    AppCastingMOOSApp::RegisterVariables();
+    for (auto &m: mixers) m.subscribe(this);
 }
 
 
 //------------------------------------------------------------
 // Procedure: buildReport()
 
-bool MixAmp::buildReport() 
+bool MixAmp::buildReport()
 {
   m_msgs << "============================================ \n";
-  m_msgs << "File:                                        \n";
+  m_msgs << " pMixAmp                                     \n";
   m_msgs << "============================================ \n";
 
-  ACTable actab(4);
-  actab << "Alpha | Bravo | Charlie | Delta";
+  list<string> headers;
+  list<string> values;
+
+  for (auto &m: mixers) {
+      list<string> tmph = m.buildReportHeader();
+      for (auto &h: tmph) headers.push_back(h);
+      list<string> tmpv = m.buildReportLines();
+      for (auto &v: tmpv) values.push_back(v);
+  }
+
+  // Assemble the response
+  ACTable actab(headers.size());
+  for (auto &h: headers) actab << h;
   actab.addHeaderLines();
-  actab << "one" << "two" << "three" << "four";
+  for (auto &v: values) actab << v;
   m_msgs << actab.getFormattedString();
 
   return(true);
 }
-
-
-
-
--- MixAmp.h
+++ MixAmp.h
@@ -9,28 +9,72 @@
 #define MixAmp_HEADER
 
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include <vector>
+#include <memory>
+#include <string>
 
-class MixAmp : public AppCastingMOOSApp
-{
- public:
-   MixAmp();
-   ~MixAmp();
+class Mixer;    // Forward declaration
 
- protected: // Standard MOOSApp functions to overload  
-   bool OnNewMail(MOOSMSG_LIST &NewMail);
-   bool Iterate();
-   bool OnConnectToServer();
-   bool OnStartUp();
+class MixAmp : public AppCastingMOOSApp {
+    public:
+        MixAmp() {};
+        ~MixAmp() {};
+        bool notify(std::string var, double val) {
+            return Notify(var, val);
+        };
+        bool registerVar(std::string var) {
+            return Register(var);
+        };
 
- protected: // Standard AppCastingMOOSApp function to overload 
-   bool buildReport();
+    protected: // Standard MOOSApp functions to overload
+        bool OnNewMail(MOOSMSG_LIST &NewMail);
+        bool Iterate();
+        bool OnConnectToServer();
+        bool OnStartUp();
 
- protected:
-   void registerVariables();
+        // Standard AppCastingMOOSApp function to overload
+        bool buildReport();
+        void registerVariables();
 
- private: // Configuration variables
-
- private: // State variables
+    private: // State variables
+        std::vector<Mixer> mixers;
 };
 
-#endif 
+class MixAmpInput {
+    public:
+        MixAmpInput(rapidjson::Value &v);
+        bool procMail(CMOOSMsg &msg);
+        bool subscribe(MixAmp* m) {m->registerVar(var);};
+        double raw() {return val;};
+        double cooked() {return ((val + offset) * gain);};
+        const std::string &getVar() {return var;};
+
+    private:
+        double gain = 1;
+        double offset = 0;
+        double val = 0;
+        std::string var = "";
+};
+
+class Mixer {
+    public:
+        Mixer(rapidjson::Value &v);
+        bool procMail(CMOOSMsg &msg);
+        bool subscribe (MixAmp *m) {for (auto &i: inputs) i.subscribe(m);};
+        bool transmit (MixAmp *m) {return m->notify(var, cooked());};
+        std::list<std::string> buildReportHeader();
+        std::list<std::string> buildReportLines();
+        double raw() {return output;};
+        double cooked() {return ((output + offset) * gain);};
+
+    private:
+        double gain = 1;
+        double offset = 0;
+        double output = 0;
+        std::string var = "";
+        std::vector<MixAmpInput> inputs;
+};
+
+#endif
